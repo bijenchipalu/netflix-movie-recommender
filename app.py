a1a97e46209cb6a018c5ebb6276220dff5c6650e
bijenchipalu <bijenchipalu@gmail.com>
2026-03-30 00:24:25 +0545

    Add files via upload

--- app.py
+++ app.py
@@ -1,50 +1,36 @@
 import streamlit as st
 import pickle
-import ast
-import requests
-import os
-import gdown
+import pandas as pd
 
-if not os.path.exists("similarity.pkl") or os.path.getsize("similarity.pkl") < 1000000:
-    with st.spinner("Downloading similarity file, please wait..."):
-        gdown.download("https://drive.google.com/uc?id=1OBs-6uB-wyW-rHogZQDIVViEDVGHpCwb", "similarity.pkl", quiet=False, fuzzy=True)
 
-movies  = pickle.load(open('movie.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
-             
-
-def fetch_poster(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=YOUR_API_KEY"
-    data = requests.get(url).json()
-    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
-
+# Function to recommend movies
 def recommend(movie):
-    index=movies[movies['title']==movie].index[0]
-    distances = similarity[index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True,key=lambda x:x[1])[1:6]
+  movie_index = movies[movies['title'] == movie].index[0]
+  distances = similarity[movie_index]
+  movies_list = sorted(list(enumerate(distances)),reverse=True, key=lambda x:x[1])[1:6]
 
-    names = []
-    posters = []
+  recommended_movies = []
+  for i in movies_list:
+      recommended_movies.append(movies.iloc[i[0]].title)
+  
+  return recommended_movies
 
-    for i in movies_list:
-        movie_id =movies.iloc[i[0]].id
-        names.append(movies.iloc[i[0]].title)
-        posters.append(fetch_poster(movie_id))
-    return names, posters
+
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
 
 
 st.title('Movie Recommender System')
-selected_movie = st.selectbox(
-    "Select a movie",
-    movies['title'].values
+
+selected_movie_name = st.selectbox(
+  'List of the movies',
+  movies['title'].values
 )
 
 if st.button('Recommend'):
-    names, posters = recommend(selected_movie)
-
-    cols = st.columns(5)
-
-    for i in range(5):
-        with cols[i]:
-            st.text(names[i])
-            st.image(posters[i])
+  recommendations = recommend(selected_movie_name)
+  for i in recommendations:
+    st.write(i)
